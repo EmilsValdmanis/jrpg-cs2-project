48bc3a91edcfe409dfbf944c7135a1855b0934b1
Emīls Valdmanis <valdmanis71@gmail.com>
2022-02-10 13:56:24 +0200

    Finished Tank class

--- src/main/Tank.scala
+++ src/main/Tank.scala
@@ -24,4 +24,58 @@ class Tank(nickname: String) extends Character(nickname){
     this.Armor += 10
     this.MR += 10
   }
+
+  def smash(opponent: Character): Unit= {
+    this.attackAD(opponent)
+    opponent.currentHP += 10
+    opponent.Armor -= 3
+    opponent.MR -= 3
+    this.currentHP -= this.level * 10
+    this.Armor -= 4
+    this.MR -= 4
+  }
+
+  def feast(): Unit= {
+    if (this.level >=10){
+      this.currentHP += this.level * 30
+      this.Armor += 5
+      this.MR += 5
+      this.AP -= 5
+    }
+  }
+
+  def deathSentence(opponent: Character): Unit= {
+    if (this.level >= 15){
+      opponent.currentHP -= opponent.MR
+      this.attackAP(opponent)
+      this.attackAP(opponent)
+      this.currentHP -= 40
+      this.currentMana -= 5
+    }
+  }
+
+  def battleOptions(): List[String] = {
+    if (this.level >= 15) {
+      List("smash", "feast", "deathSentence")
+    }else if (this.level >= 10) {
+      List("smash", "feast")
+    }else{
+      List("smash")
+    }
+  }
+
+  def takeAction(action: String, character: Character): Unit ={
+    val options = this.battleOptions()
+    for (possible <- options){
+      if (action == possible){
+        if (action == "smash"){
+          this.smash(character)
+        }else if (action == "feast"){
+          this.feast()
+        }else if (action == "deathSentence"){
+          this.deathSentence(character)
+        }
+      }
+    }
+  }
 }
